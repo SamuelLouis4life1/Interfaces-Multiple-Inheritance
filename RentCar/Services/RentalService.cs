0ea52292a291cbe75b8f7268cffb94b1b88c7b50
Samuel Louis <sampeur4life1@gmail.com>
2020-02-01 17:36:40 -0300

    first commit in the rent car aplication without interface4

---
+++ RentCar/Services/RentalService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using RentCar.Entities;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RentCar.Services
+{
+    class RentalService
+    {
+        public double PricePerHour { get; set; }
+        public double PricePerDay { get; set; }
+
+        private BrazilTaxService _brazilTaxService = new BrazilTaxService(); 
+
+        public RentalService(double pricePerHour, double pricePerDay)
+        {
+            PricePerHour = pricePerHour;
+            PricePerDay = pricePerDay;
+        }
+
+        public void ProcessInvoice(CarRental carRental)
+        {
+            TimeSpan duration = carRental.Finish.Subtract(carRental.Start);
+
+            double basicPayment = 0.0;
+            if (duration.TotalHours <= 12.0)
+            {
+                basicPayment = PricePerHour * Math.Ceiling(duration.TotalHours);
+            }
+            else
+            {
+                basicPayment = PricePerDay * Math.Ceiling(duration.TotalDays);
+            }
+
+            double tax = _brazilTaxService.Tax(basicPayment);
+
+            carRental.Invoice = new Invoice(basicPayment, tax);
+        }
+    }
+}
