4cf02fb9282784244a49252744f3ca7c9cd444c7
Samuel Louis <sampeur4life1@gmail.com>
2020-02-01 23:41:24 -0300

    commit a program to read a hardcode file containing people's names, storing them in a list. Then, sort the data in that list

---
+++ MotivatingProblem/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace MotivatingProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\sa\Documents\New folder\file4.txt";
+            try
+            {
+                using (StreamReader streamReader = File.OpenText(path))
+                {
+                    List<string> list = new List<string>();
+                    while (!streamReader.EndOfStream)
+                    {
+                        list.Add(streamReader.ReadLine());
+                    }
+                    list.Sort();
+                    foreach (string str in list)
+                    {
+                        Console.WriteLine(str);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occured");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
